30ba2918b9ed18a86145c68ef718512b25d16a50
bka-2cycle <114967375+bka-2cycle@users.noreply.github.com>
2023-01-11 21:37:49 -0800

    javascript finalized, Readme created, CSS revised, and code tested

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # Team-Profile-Generator
-A Node.js command-line application that takes in information about employees on a software engineering team, then generates an HTML webpage that displays summaries for each person.
+
+## Description
+
+A Node.js command-line application that takes in information about employees on a software engineering team, then generates an HTML webpage that displays summaries for each person. It provides a good way to track teams in a work environment. It alows for quick review and contact points for teams and their members. The project allowed me to practice OOP in the node environment and dynamically create web pages using command line prompts for content.
+
+## Installation
+
+The app runs via the command line, using Node index.js.
+
+## Usage
+
+Initiate prompts to build a team by running Node index. First create the manager position, then add as many engineers and interns as needed. Once the prompts are finished, an index.html file is created that when loded in a browser will display cards for each employee with links for contact and online github profiles and other inter-office info.
+
+![image of index.html displayed in browser with cards displayed](/images/screenshot-01.png)
+
+The following video shows test being run via the command line:
+https://watch.screencastify.com/v/s2TFcJZyRy2cwRi3AcW7
+
+The following video show the app in use from beginning to end:
+https://drive.google.com/file/d/1_mrtAx95Jcy_LGHr_eeuctUB8mvooiTm/view
+
+The following video shows email and GitHub links functioning:
+https://drive.google.com/file/d/12b5FZrjbEFhkYKuJJJXOf-foGqz1JUrG/view
+
+## Credits
+
+N/A
+
+## License
+
+MIT, Please refer to the license in the Repo.
+
+
+
+
+
+
+
+
+
--- index.js
+++ index.js
@@ -141,14 +141,19 @@ optQuestion = function() {
         crossorigin="anonymous">
     
         <!-- Google Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Bakbak+One&family=Yeseva+One&display=swap" rel="stylesheet">
     
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Bakbak+One&family=Permanent+Marker&display=swap" rel="stylesheet">
+
         <link rel="stylesheet" href="./style.css" />
     
         <title>Team Profile Generator</title>
     </head>
     <body>
     <header>
-    <h1>My Team</h1>
+    <h1>MY TEAM</h1>
     </header>
     <div id='all-cards'>
     ${cardHtml(employeeArray)}
@@ -191,11 +196,13 @@ function cardHtml (data) {
             htmlMarkup +=
 
 `<div class="card" style="width: 18rem;">
-  <div class="card-header">&#xF18F; ${data[i].getRole()}</div>
+  <div class="card-header"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-binoculars-fill" viewBox="0 0 16 16">
+  <path d="M4.5 1A1.5 1.5 0 0 0 3 2.5V3h4v-.5A1.5 1.5 0 0 0 5.5 1h-1zM7 4v1h2V4h4v.882a.5.5 0 0 0 .276.447l.895.447A1.5 1.5 0 0 1 15 7.118V13H9v-1.5a.5.5 0 0 1 .146-.354l.854-.853V9.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v.793l.854.853A.5.5 0 0 1 7 11.5V13H1V7.118a1.5 1.5 0 0 1 .83-1.342l.894-.447A.5.5 0 0 0 3 4.882V4h4zM1 14v.5A1.5 1.5 0 0 0 2.5 16h3A1.5 1.5 0 0 0 7 14.5V14H1zm8 0v.5a1.5 1.5 0 0 0 1.5 1.5h3a1.5 1.5 0 0 0 1.5-1.5V14H9zm4-11H9v-.5A1.5 1.5 0 0 1 10.5 1h1A1.5 1.5 0 0 1 13 2.5V3z"/>
+</svg> ${data[i].getRole()}</div>
   <ul class="list-group list-group-flush">
-    <li class="list-group-item">${data[i].getName()}</li>
+    <li id="the-name" class="list-group-item">${data[i].getName()}</li>
     <li class="list-group-item">ID: ${data[i].getId()}</li>
-    <li class="list-group-item">Email: ${data[i].getEmail()}</li>
+    <li class="list-group-item">Email: <a href="mailto:${data[i].getEmail()}">${data[i].getEmail()}</a></li>
     <li class="list-group-item">Office #: ${data[i].getOfficeNumber()}</li>
 </div>`
 
@@ -204,23 +211,28 @@ function cardHtml (data) {
             htmlMarkup +=
 
 `<div class="card" style="width: 18rem;">
-  <div class="card-header">&#xF4B2; ${data[i].getRole()}</div>
+  <div class="card-header"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-rulers" viewBox="0 0 16 16">
+  <path d="M1 0a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h5v-1H2v-1h4v-1H4v-1h2v-1H2v-1h4V9H4V8h2V7H2V6h4V2h1v4h1V4h1v2h1V2h1v4h1V4h1v2h1V2h1v4h1V1a1 1 0 0 0-1-1H1z"/>
+</svg> ${data[i].getRole()}</div>
   <ul class="list-group list-group-flush">
-    <li class="list-group-item">${data[i].getName()}</li>
+    <li id="the-name" class="list-group-item">${data[i].getName()}</li>
     <li class="list-group-item">ID: ${data[i].getId()}</li>
-    <li class="list-group-item">Email: ${data[i].getEmail()}</li>
-    <li class="list-group-item">GitHub:${data[i].getGithub()}</li>
+    <li class="list-group-item">Email: <a href="mailto:${data[i].getEmail()}">${data[i].getEmail()}</a></li>
+    <li class="list-group-item">GitHub: <a href="https://github.com/${data[i].getGithub()}" target="_blank">https://github.com/${data[i].getGithub()}</a></li>
 </div>`
         }
         if (data[i].getRole() === "Intern") {
             htmlMarkup +=
 
 `<div class="card" style="width: 18rem;">
-  <div class="card-header">&#xF6FE; ${data[i].getRole()}</div>
+  <div class="card-header"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-mortarboard-fill" viewBox="0 0 16 16">
+  <path d="M8.211 2.047a.5.5 0 0 0-.422 0l-7.5 3.5a.5.5 0 0 0 .025.917l7.5 3a.5.5 0 0 0 .372 0L14 7.14V13a1 1 0 0 0-1 1v2h3v-2a1 1 0 0 0-1-1V6.739l.686-.275a.5.5 0 0 0 .025-.917l-7.5-3.5Z"/>
+  <path d="M4.176 9.032a.5.5 0 0 0-.656.327l-.5 1.7a.5.5 0 0 0 .294.605l4.5 1.8a.5.5 0 0 0 .372 0l4.5-1.8a.5.5 0 0 0 .294-.605l-.5-1.7a.5.5 0 0 0-.656-.327L8 10.466 4.176 9.032Z"/>
+</svg> ${data[i].getRole()}</div>
   <ul class="list-group list-group-flush">
-    <li class="list-group-item">${data[i].getName()}</li>
+    <li id="the-name" class="list-group-item">${data[i].getName()}</li>
     <li class="list-group-item">ID: ${data[i].getId()}</li>
-    <li class="list-group-item">Email: ${data[i].getEmail()}</li>
+    <li class="list-group-item">Email: <a href="mailto:${data[i].getEmail()}">${data[i].getEmail()}</a></li>
     <li class="list-group-item">School: ${data[i].getSchool()}</li>
 </div>`
         }
